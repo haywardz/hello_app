950c0225943489e28464d389defa8c216b6f162b
haywardz <mhayward@blancdigital.com>
2017-07-18 12:34:39 +1200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello to the world"
+    render html: "hello to the world and James"
   end
 end
